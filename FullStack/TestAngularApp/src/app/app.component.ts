101b3651da64b66f1292dba77b5ec9eb62a3b7b4
Ominousity <abs@hotmail.dk>
2023-04-13 16:44:09 +0200

    created simple chat program

--- FullStack/TestAngularApp/src/app/app.component.ts
+++ FullStack/TestAngularApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FireService } from './fire.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TestAngularApp';
+  sendThis: any;
+
+  constructor(public fireservice: FireService) {
+    
+  }
+
+  
 }
---
+++ FullStack/TestAngularApp/src/app/fire.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+import * as config from '../../firebaseconfig.js';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FireService {
+
+  firebaseApplication;
+  firestore: firebase.firestore.Firestore;
+  messages: any[] = [];
+
+  constructor() { 
+    this.firebaseApplication = firebase.initializeApp(config.firebaseConfig);
+    this.firestore = firebase.firestore();
+    this.getMessages();
+  }
+
+  sendMessage(sendMessage: any) : void{
+    let messageDTO: MessageDTO = {
+      content: sendMessage,
+      timestamp: new Date(),
+      user: 'somebody'
+    }
+    this.firestore.collection('chat')
+    .add(messageDTO)
+  }
+
+  getMessages() {
+    this.firestore
+    .collection('chat')
+    .onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(change => {
+        if (change.type == "added"){
+          this.messages.push({id: change.doc.id, data: change.doc.data()});
+        } else if (change.type == "modified"){
+          const index = this.messages.findIndex(document => document.id != change.doc.id);
+          this.messages[index] = {
+            id: change.doc.id, data: change.doc.data()
+          }
+        }
+      })
+    })
+  }
+
+}
+
+export interface MessageDTO{
+  content: string;
+  timestamp: Date;
+  user: string;
+}
